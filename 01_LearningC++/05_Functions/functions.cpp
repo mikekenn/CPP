12b7c947ad84bd2158f8b22039ae0f9d858c1a05
Michael Kennedy <michaelkennedy_228@hotmail.com>
2021-12-22 01:37:38 -0400

    Create and use functions example.

--- 01_LearningC++/05_Functions/functions.cpp
+++ 01_LearningC++/05_Functions/functions.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <vector>
+#include "../03_StructuresClassAndPointers/Student.h"
+#include "../03_StructuresClassAndPointers/Course.h"
+#include "../03_StructuresClassAndPointers/Grade.h"
+#include "../03_StructuresClassAndPointers/StudentRecords.h"
+
+void initialize();
+
+StudentRecords SR;
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`*/
+
+    initialize();
+
+    int id;
+
+    std::cout << "Enter a student ID: ";
+    std::cin >> id;
+
+    std::string student_str = SR.get_student_name(id);
+    std::cout << "The GPA for " << student_str << " is " << SR.get_GPA(id) << std::endl;
+
+    std::cout << "~~* Creating Functions Example Beginning *~~" << std::endl;
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`*/
+}
+
+void initialize(){
+    SR.add_student(1, "George P. Burdell");
+    SR.add_student(2, "Nancy Rhodes");
+
+    SR.add_course(1, "Algebra", 5);
+    SR.add_course(2, "Physics", 4);
+    SR.add_course(3, "English", 3);
+    SR.add_course(4, "Economics", 4);
+
+    SR.add_grade(1, 1, 'B');
+    SR.add_grade(1, 2, 'A');
+    SR.add_grade(1, 3, 'C');
+    SR.add_grade(2, 1, 'A');
+    SR.add_grade(2, 2, 'A');
+    SR.add_grade(2, 4, 'B');
 }
